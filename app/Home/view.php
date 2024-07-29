51f98b6a9661024fe782fc2c9913ab74a999aff2
Ubden® <44896964+ubden@users.noreply.github.com>
2024-07-29 10:48:25 +0300

    information

--- app/Home/view.php
+++ app/Home/view.php
@@ -1,3 +1,15 @@
+<?php
+// Coff Framework
+// Created by Ubden Community
+// GitHub: https://github.com/ubden/coff-framework
+// Contributors: https://github.com/ck-cankurt
+// License: GNU GENERAL PUBLIC LICENSE
+// Framerwork Website: https://coff.dev
+// Sponsored Website: https://ubden.com
+// Version: ubden/coff-framework/version.txt
+// Release Date: 2024
+?>
+
 <?php require '../includes/header.php'; ?>
 
 <h1><?php echo $message; ?></h1>
