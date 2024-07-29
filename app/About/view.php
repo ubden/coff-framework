65bc8b32ee50ed5a3330c98a7ba1aab18a9f65f1
Ubden® <44896964+ubden@users.noreply.github.com>
2024-07-29 20:18:54 +0300

    test

--- app/About/view.php
+++ app/About/view.php
@@ -14,8 +14,6 @@ require __DIR__ . '/../includes/header.php';
 
 ?>
 
-
-
 <div class="container mt-5">
     <div class="row">
         <div class="col-md-12 text-center">
--- app/Contact/view.php
+++ app/Contact/view.php
@@ -14,6 +14,29 @@ require __DIR__ . '/../includes/header.php';
 
 ?>
 
+<div class="container mt-5">
+    <h1><?php echo $message; ?></h1>
+    <p>If you have any questions, feel free to contact us by filling out the form below.</p>
+    
+    <form action="handler.php" method="post" class="mt-4">
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="form-group mt-3">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="form-group mt-3">
+            <label for="subject">Subject:</label>
+            <input type="text" class="form-control" id="subject" name="subject" required>
+        </div>
+        <div class="form-group mt-3">
+            <label for="message">Message:</label>
+            <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mt-4">Send Message</button>
+    </form>
+</div>
 
-<h1><?php echo $message; ?></h1>
 <?php require __DIR__ . '/../includes/footer.php'; ?>
