5a55159b4c3f3b7ff04eb587417ce077aa891c4c
Ubden® <44896964+ubden@users.noreply.github.com>
2024-07-29 19:27:09 +0300

    contact

--- app/Contact/handler.php
+++ app/Contact/handler.php
@@ -17,23 +17,7 @@ class Handler
 {
     public function handle()
     {
-        // Get the form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-
-// Here, you can add code to handle the form submission, such as sending an email or saving the data to a database
-
-// For now, we'll just display the submitted data
-echo "<h2>Thank you for contacting us, $name.</h2>";
-echo "<p>We have received your message and will get back to you shortly.</p>";
-echo "<p><strong>Name:</strong> $name</p>";
-echo "<p><strong>Email:</strong> $email</p>";
-echo "<p><strong>Subject:</strong> $subject</p>";
-echo "<p><strong>Message:</strong> $message</p>";
-
-
+        $message = 'Welcome to Coff Framework Contact Page!';
         require __DIR__ . '/view.php';
     }
 }
