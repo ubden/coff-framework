4fb821806790df14343bd26edd8ee4eeebc483ee
Ubden® <44896964+ubden@users.noreply.github.com>
2024-07-29 13:49:02 +0300

    infos

--- app/About/handler.php
+++ app/About/handler.php
@@ -1,4 +1,14 @@
-
+<?php
+// Coff Framework
+// Created by Ubden Community
+// GitHub: https://github.com/ubden/coff-framework
+// Contributors: https://github.com/ck-cankurt
+// License: GNU GENERAL PUBLIC LICENSE
+// Framerwork Website: https://coff.dev
+// Sponsored Website: https://ubden.com
+// Version: ubden/coff-framework/version.txt
+// Release Date: 2024
+?>
 <?php
 
 namespace App\About;
