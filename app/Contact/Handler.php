6cc5ef268ee2b008fca0df52272a1102ef461b20
Ubden® <44896964+ubden@users.noreply.github.com>
2024-07-30 08:51:47 +0300

    tesfds

--- app/Contact/Handler.php
+++ app/Contact/Handler.php
@@ -22,7 +22,6 @@ class Handler
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
-        $mailconfig = require __DIR__ . '/../../config/config.php';
         $message = 'Welcome to Coff Framework Contact Page!';
         file_put_contents(__DIR__.'/../../logs/debug.log', "Handler Invoked." . PHP_EOL, FILE_APPEND);
 
