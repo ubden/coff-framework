78e4a3895b5e31a9b6f7789b42052331a8642a68
Ubden® <44896964+ubden@users.noreply.github.com>
2024-07-30 09:27:37 +0300

    handler

--- app/Contact/Handler.php
+++ app/Contact/Handler.php
@@ -1,5 +1,24 @@
 <?php
-// Handler sınıfına mesaj bilgisi ekleyin
+// Coff PHP Framework
+// Created by Ubden Community
+// GitHub: https://github.com/ubden/coff-framework
+// Contributors: https://github.com/ck-cankurt
+// License: GNU GENERAL PUBLIC LICENSE
+// Framework Website: https://coff.dev
+// Sponsored Website: https://ubden.com
+// Version: ubden/coff-framework/version.txt
+// Release Date: 2024
+
+namespace App\Contact;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Error reporting for debugging (remove or modify in production)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 class Handler
 {
     private $config;
@@ -64,6 +83,8 @@ class Handler
         }
     }
 
+    // Smtp ayarlarını yapılandıran metod
+
     private function configureMailer(PHPMailer $mail)
     {
         $mail->isSMTP();
@@ -76,6 +97,7 @@ class Handler
     }
 
     // Mesajları ve mesaj türünü döndüren getter metodları
+    
     public function getMessage()
     {
         return $this->message;
