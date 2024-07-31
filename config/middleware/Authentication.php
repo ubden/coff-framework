98fa3f7b7f54d61493b7a5f4ac37e66e49a8a9d0
Ubden® <44896964+ubden@users.noreply.github.com>
2024-07-31 09:14:21 +0300

    auth

--- config/middleware/Authentication.php
+++ config/middleware/Authentication.php
@@ -4,10 +4,14 @@ namespace App\Middleware;
 
 class Authentication {
     public function handle() {
+        // Oturum başlatma kontrolü
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        
         // Örnek doğrulama kontrolü
         if (!isset($_SESSION['user'])) {
-            http_response_code(401);
-            echo '401 Unauthorized';
+            header('Location: /login.php');
             exit;
         }
     }
